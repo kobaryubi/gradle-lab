2809e2f72a7da97ed63e7870d55b87ff0de6657c
Masahiko Kobayashi <kobaryubi6@gmail.com>
2024-10-17 18:25:35 +0900

    feat: exec

--- gatling/src/gatling/kotlin/computerdatabase/ComputerDatabaseSimulation.kt
+++ gatling/src/gatling/kotlin/computerdatabase/ComputerDatabaseSimulation.kt
@@ -1,13 +1,19 @@
 package computerdatabase
 
-import io.gatling.javaapi.core.Simulation
 import io.gatling.javaapi.core.CoreDsl.*
+import io.gatling.javaapi.core.Simulation
 import io.gatling.javaapi.http.HttpDsl.*
 
 class ComputerDatabaseSimulation : Simulation() {
-    val httpProtocolBuilder = http.baseUrl("https://computer-database.gatling.io")
+    private val httpProtocolBuilder = http.baseUrl("https://computer-database.gatling.io")
         .acceptHeader("text/html")
 
-    val scenarioBuilder = scenario("Scenario")
+    private val scenarioBuilder = scenario("Scenario")
         .exec(http("Computers").get("/computers"))
+
+    init {
+        setUp(scenarioBuilder.injectOpen(constantUsersPerSec(1.0).during(60)))
+            .protocols(httpProtocolBuilder)
+    }
 }
+
